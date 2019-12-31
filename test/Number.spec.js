8283be032158648338190a2e393732e9d2c2f3ad
Chris Isler <christopherisler1@gmail.com>
2019-12-31 10:11:24 -0600

    Number tests

--- test/Number.spec.js
+++ test/Number.spec.js
@@ -1,12 +1,31 @@
 import test from 'ava';
 import { wavematch } from '../dist/wavematch.cjs.development';
 
-test('Number', t => {
+test('Negative  Numbers', t => {
+    // Guard
+    // TODO
+    // Literal
+    wavematch(-42)(
+        (s = -42) => t.pass(),
+        _ => t.fail()
+    );
+    wavematch(-3)(
+        (s = 0) => t.fail(),
+        (s = 79) => t.fail(),
+        _ => t.pass()
+    );
+    // Typed
+    wavematch(-33)(
+        (s = Number) => t.pass(),
+        _ => t.fail()
+    );
+});
+
+test('Positive Numbers', t => {
     // Guard
     // TODO
     // Literal
     wavematch(42)(
-        (s = -42) => t.fail(),
         (s = 42) => t.pass(),
         _ => t.fail()
     );
@@ -22,14 +41,24 @@ test('Number', t => {
     );
 });
 
-// test('Non-number', t => {
-//     // Guard
-//     // TODO
-//     // Literal
-//     // TODO
-//     // Typed
-//     // TODO
-// });
+test('Non-number', t => {
+    [{}, () => {}, '42', Symbol(), Error(), false, []].forEach(notANumber => {
+        // Guard
+        // TODO
+        // Literal
+        wavematch(notANumber)(
+            (s = -1) => t.fail(),
+            (s = 0) => t.fail(),
+            (s = 42) => t.fail(),
+            _ => t.pass()
+        );
+        // Typed
+        wavematch(notANumber)(
+            (s = Number) => t.fail(),
+            _ => t.pass()
+        );
+    });
+});
 
 test('Zero', t => {
     // TODO Uncomment as feature coverage increases
