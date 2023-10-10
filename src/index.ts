024b11ac2782955e5625ea47cd7d2c72f038e2a8
Chris Isler <christopherisler1@gmail.com>
2023-10-09 23:18:02 -0500

    fix tsdx's outdated TS compiler issues

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,20 @@ import { Patterns } from './patterns';
  * given branch, each default argument constitutes a special pattern describing
  * the kind of input data that branch expects.
  */
-export const wavematch = (...args: unknown[]) => <U>(...branches: ((...xs: any[]) => U)[]): U => {
-    if (args.length === 0) throw Error('Invariant: No data');
-    if (branches.length === 0) throw Error('Invariant: No branches');
-    for (let index = 0; index < branches.length; index++) {
-        const branch = branches[index];
-        if (doesMatch(args, branch)) {
-            return branch(...args);
+export function wavematch<const T>(...args: T[]) {
+    return <U>(...branches: ((...xs: T[]) => U)[]): U => {
+        if (args.length === 0) throw Error('Invariant: No data');
+        if (branches.length === 0) throw Error('Invariant: No branches');
+        for (let index = 0; index < branches.length; index++) {
+            const branch = branches[index];
+            if (doesMatch(args, branch)) return branch(...args);
         }
-    }
-    // Return the last branch, assumed to be the default behavior
-    return branches[branches.length - 1]();
-};
+        // Return the last branch, assumed to be the default behavior
+        return branches[branches.length - 1]();
+    };
+}
 
-function doesMatch(args: readonly unknown[], branch: Function): boolean {
+function doesMatch(args: readonly unknown[], branch: () => unknown): boolean {
     const ast = parseExpression(branch.toString());
     if (ast.type !== 'ArrowFunctionExpression') {
         throw TypeError(`Expected an arrow function. Received: ${ast}`);
